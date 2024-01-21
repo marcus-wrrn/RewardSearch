fixed bug causing inaccurate neutral weighting

src/train_codenames.py
@@ -2,7 +2,7 @@ import torch
 from torch.optim.lr_scheduler import ExponentialLR
 from loss_fns.loss import  RewardSearchLoss
 from torch.utils.data import DataLoader
-from models.multi_objective_models import MORSpyMaster
+from models.multi_objective_models import MORSpyMaster, MORSpyWasserstein
 from datasets.dataset import CodeNamesDataset
 import numpy as np
 import datetime
@@ -144,7 +144,7 @@ def main(args):
     valid_dataloader = DataLoader(valid_dataset, batch_size=50, num_workers=4)
 
     vector_db = VectorSearch(train_dataset, prune=True)
-    model = MORSpyMaster(vector_db, device, vocab_size, search_pruning=search_pruning)
+    model = MORSpyMaster(vector_db, device, vocab_size=vocab_size, search_pruning=search_pruning)
     model.to(device)
 
     losses_train, losses_valid = train(n_epochs=args.e, model=model, train_loader=train_dataloader, valid_dataloader=valid_dataloader, device=device, model_path=model_out, normalize_reward=normalize_reward)
@@ -158,7 +158,7 @@ if __name__ == "__main__":
     parser.add_argument('-guess_data', type=str, help="Geuss words dataset path", default=BASE_DIR + "data/codewords_full_w_assassin_valid.json")
     parser.add_argument('-vocab', type=int, default=80)
     parser.add_argument('-weight_decay', type=float, default=0.1)
-    parser.add_argument('-prune_search', type=str, help="Prunes the search window based on average similarity [Y/n]", default='N')
+    parser.add_argument('-prune_search', type=str, help="Prunes the search window based on average similarity [Y/n]", default='Y')
     parser.add_argument('-gamma', type=float, default=0.9)
     parser.add_argument('-norm', type=str, help="Whether to normalize reward function, [Y/n]", default='Y')
     parser.add_argument('-val_guess_data', type=str, help="Filepath for the validation dataset", default=BASE_DIR + "data/codewords_full_w_assassin_mini.json")