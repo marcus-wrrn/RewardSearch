a591a36f94cffe848ce76fdb5ce3e12eecd55eec
marcus-wrrn <marcuswrrn@gmail.com>
2024-01-21 16:38:38 -0500

    First implementation of wasserstein rotation

--- src/models/multi_objective_models.py
+++ src/models/multi_objective_models.py
@@ -75,7 +75,7 @@ class MORSpyMaster(nn.Module):
 
         if reverse:
             # Find the inverse of the positive reward (num incorrect)
-            return pos_reward.shape[1] - num_correct
+            return pos_reward.shape[0] - num_correct
         
         return num_correct
 
@@ -150,7 +150,7 @@ class MORSpyMaster(nn.Module):
         model_out_expanded = model_out.unsqueeze(1)
         # Calculate similarity
         sim_scores = F.cosine_similarity(word_embeddings, model_out_expanded, dim=2)
-        # Find average batch score
+        # Find std deviation of batch score
         std_dev = sim_scores.std(dim=1).mean()
         # Create pruning algorithm
 
@@ -187,7 +187,7 @@ class MORSpyWasserstein(MORSpyMaster):
     def __init__(self, vocab: VectorSearch, device: device, neutral_weight=1, negative_weight=0, assas_weights=-10, backbone='all-mpnet-base-v2', vocab_size=80, search_pruning=False):
         super().__init__(vocab, device, neutral_weight, negative_weight, assas_weights, backbone, vocab_size, search_pruning)
     
-    def wasserstein_rot(self, pos_dist: Tensor, pos_weights: Tensor, neg_dist: Tensor, neg_weights: Tensor):
+    def _wasserstein_rot(self, pos_dist: Tensor, pos_weights: Tensor, neg_dist: Tensor, neg_weights: Tensor):
         """Wasserstein Rotation between the normalized scores of both positive and negative distributions"""
         # # Sort the distributions and their corresponding weights
         # sorted_indices1 = torch.argsort(pos_dist)
@@ -201,19 +201,41 @@ class MORSpyWasserstein(MORSpyMaster):
         all_values = torch.cat((pos_dist, neg_dist), dim=1)
 
         # Might not need this
-        unique_values, indices = torch.unique(all_values, sorted=True, return_inverse=True)
-
-        cdf1 = torch.zeros(len(unique_values), dtype=torch.float)
-        cdf2 = torch.zeros(len(unique_values), dtype=torch.float)
-
-        cdf1[indices[:len(pos_dist)]] = pos_weights
-        cdf2[indices[len(pos_dist):]] = neg_weights
+        #unique_values, indices = torch.unique(all_values, sorted=True, return_inverse=True)
 
         # Compute the Wasserstein distance
-        distance = torch.sum(torch.abs(cdf1 - cdf2) * torch.diff(unique_values))
+        distance = torch.sum(torch.abs(pos_weights - neg_weights) * (neg_dist - pos_dist), dim=1)
 
         return distance
     
+    def find_search_embeddings(self, word_embeddings: Tensor, pos_encs: Tensor, neg_encs: Tensor, neut_encs: Tensor, assassin_encs: Tensor):
+        word_embs_expanded = word_embeddings.unsqueeze(2)
+        # Process encoding shapes
+        pos_encs = self._expand_encodings_for_search(pos_encs)
+        neg_encs = self._expand_encodings_for_search(neg_encs)
+        neut_encs = self._expand_encodings_for_search(neut_encs)
+        assas_encs = self._expand_encodings_for_search(assassin_encs.unsqueeze(1))
+
+        pos_reward = self._get_total_reward(word_embs_expanded, pos_encs, neg_encs, neut_encs, assas_encs, reverse=False)
+        neg_reward = self._get_total_reward(word_embs_expanded, pos_encs, neg_encs, neut_encs, assas_encs, reverse=True)
+
+        # Normalize positive and negative rewards
+        #pos_reward_sum = pos_reward.sum(dim=1, keepdim=True)
+        pos_reward_sorted, pos_reward_indices = torch.sort((pos_reward), dim=1)
+
+        #neg_reward_sum = neg_reward.sum(dim=1, keepdim=True)
+        neg_reward_sorted, neg_reward_indices = torch.sort((neg_reward), dim=1)
+
+        embeddings_pos = torch.gather(word_embeddings, 1, pos_reward_indices.unsqueeze(-1).expand(-1, -1, word_embeddings.shape[2]))
+        embeddings_neg = torch.gather(word_embeddings, 1, neg_reward_indices.unsqueeze(-1).expand(-1, -1, word_embeddings.shape[2]))
+
+        # Highest scoring word embedding
+        highest_scoring_embedding = embeddings_pos[:, 0]
+        highest_scoring_embedding_index = pos_reward_indices[:, 0]
+
+
+        return highest_scoring_embedding, highest_scoring_embedding_index, (embeddings_pos, embeddings_neg), (pos_reward_sorted, neg_reward_sorted)
+
     def forward(self, pos_embs: Tensor, neg_embs: Tensor, neut_embs: Tensor, assas_emb: Tensor) -> MOROutObj | tuple:
         concatenated = self._get_combined_input(pos_embs, neg_embs, neut_embs, assas_emb)
         model_out = self.fc(concatenated)
@@ -224,15 +246,19 @@ class MORSpyWasserstein(MORSpyMaster):
         words, word_embeddings, dist = self.vocab.search(model_out, num_results=self.vocab_size)
         word_embeddings = torch.tensor(word_embeddings).to(self.device).squeeze(1)
 
-        if self.search_pruning:
-            self._prune_word_embeddings(word_embeddings, model_out)
+        search_out, search_out_index, embedding_dist, reward_dist = self.find_search_embeddings(word_embeddings, pos_embs, neg_embs, neut_embs, assas_emb)
 
-        search_out, search_out_index, search_out_max, search_out_min = self.find_search_embeddings(word_embeddings, pos_embs, neg_embs, neut_embs, assas_emb)
+        # Find cosine_similarity scores
+        word_embeddings_pos, word_embeddings_neg = embedding_dist
+        pos_reward_dist, neg_reward_dist = reward_dist
 
-        if self.training:
-            return model_out, search_out, search_out_max, search_out_min
+        pos_rot = F.cosine_similarity(model_out.unsqueeze(1), word_embeddings_pos, dim=2)
+        neg_rot = F.cosine_similarity(model_out.unsqueeze(1), word_embeddings_neg, dim=2)
+
+        wasserstein_rot = self._wasserstein_rot(pos_rot, pos_reward_dist, neg_rot, neg_reward_dist)
+
+        return model_out, search_out, wasserstein_rot
         
-        return MOROutObj(words[search_out_index.cpu()][:, :1], model_out, search_out, search_out_max, search_out_min)
 
 
 # class MORSpyDualHead(MORSpyMaster):
