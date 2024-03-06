4e3b12b2c22277e62d3178fccc15c2470b67475b
marcus-wrrn <marcuswrrn@gmail.com>
2024-03-05 19:31:05 -0500

    added support for longer texts

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -1,7 +1,6 @@
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 import torch
 import json
-from sentence_transformers import SentenceTransformer
 import random
 
 class CodeGiverDataset(Dataset):
@@ -11,22 +10,26 @@ class CodeGiverDataset(Dataset):
         self.code_dir = code_dir
         self.game_dir = game_dir
 
-        with open(self.code_dir, 'r') as fp:
-            self.word_data = json.load(fp)
-        self.code_dict = self._create_word_dict(self.word_data)
-        self.guess_dict = self._create_guess_dict(self.word_data)
+        self.text_data = self._load_data(self.code_dir)
+        self.code_dict = self._create_board_dict(self.text_data)
+        self.vocab_dict = self._create_guess_dict(self.text_data)
         
         with open(self.game_dir, 'r') as fp:
             self.game_data = json.load(fp)
         
-        self._process_game_data(self.game_data)        
+        self._process_game_data(self.game_data) 
+
+    def _load_data(self, codename_dir: str):
+        with open(codename_dir, 'r') as fp:
+            filedata = json.load(fp)
+        return filedata       
     
     # Intializers
-    def _create_word_dict(self, data: json):
-        return { word: embedding for word, embedding in zip(data['codewords'], data['code_embeddings']) }
+    def _create_board_dict(self, data: json):
+        return { text: embedding for text, embedding in zip(data['codewords'], data['code_embeddings']) }
     
     def _create_guess_dict(self, data: json):
-        return { word: embedding for word, embedding in zip(data['guesses'], data['guess_embeddings']) }
+        return { text: embedding for text, embedding in zip(data['guesses'], data['guess_embeddings']) }
     
     def _process_game_data(self, data: json):
         self.positive_sents = data['positive']
@@ -37,7 +40,7 @@ class CodeGiverDataset(Dataset):
     def get_vocab(self, guess_data=True):
         words = []
         embeddings = []
-        data = self.guess_dict.items() if guess_data else self.code_dict.items()
+        data = self.vocab_dict.items() if guess_data else self.code_dict.items()
         for key, value in data:
             words.append(key)
             embeddings.append(value)
@@ -47,7 +50,7 @@ class CodeGiverDataset(Dataset):
         """Returns all words/embeddings in the guess dataset that are not in the codename dataset"""
         words = []
         embeddings = []
-        guess_data = self.guess_dict.items()
+        guess_data = self.vocab_dict.items()
         for key, value in guess_data:
             # If word exists in the code words set remove it
             if key in self.code_dict:
@@ -68,7 +71,6 @@ class CodeGiverDataset(Dataset):
         neg_embeddings = torch.stack([torch.tensor(self.code_dict[word]) for word in neg_sent.split(' ')])
 
         return pos_sent, neg_sent, pos_embeddings, neg_embeddings
-    
 
 class CodeGiverDatasetCombinedSent(CodeGiverDataset):
     def __init__(self, code_dir: str, game_dir: str):
@@ -110,9 +112,9 @@ class CodeNamesDataset(CodeGiverDataset):
     """
     Dataset for the full game of Codenames, containing both positive, negative, neutral and the assasin words
     """
-    def __init__(self, code_dir: str, game_dir: str):
+    def __init__(self, code_dir: str, game_dir: str, seperator=' '):
         super().__init__(code_dir, game_dir)
-
+        self.sep = seperator
         self.assassin_words = self.game_data['assassin']
 
     def __getitem__(self, index):
@@ -122,22 +124,24 @@ class CodeNamesDataset(CodeGiverDataset):
         assassin_word = self.assassin_words[index]
 
         # Get embeddings
-        pos_embeddings = torch.stack([torch.tensor(self.code_dict[word]) for word in pos_sent.split(' ')])
-        neg_embeddings = torch.stack([torch.tensor(self.code_dict[word]) for word in neg_sent.split(' ')])
-        neutral_embeddings = torch.stack([torch.tensor(self.code_dict[word]) for word in neutral_sent.split(' ')])
+        pos_embeddings = torch.stack([torch.tensor(self.code_dict[word]) for word in pos_sent.split(self.sep)])
+        neg_embeddings = torch.stack([torch.tensor(self.code_dict[word]) for word in neg_sent.split(self.sep)])
+        neutral_embeddings = torch.stack([torch.tensor(self.code_dict[word]) for word in neutral_sent.split(self.sep)])
         assassin_embedding = torch.tensor(self.code_dict[assassin_word])
 
         return (pos_sent, neg_sent, neutral_sent, assassin_word), (pos_embeddings, neg_embeddings, neutral_embeddings, assassin_embedding)
 
-def testing_dataloader():
-    dataset = CodeGiverDataset(code_dir="../data/words.json", game_dir="../data/three_word_data.json")
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
-    model = SentenceTransformer('all-mpnet-base-v2')
+class SentenceNamesDataset(CodeNamesDataset):
+    # TODO: Refactor, this does not follow best practices at all
+    def __init__(self, code_dir: str, game_dir: str, vocab_dir: str, seperator='<SEP>'):
+        super().__init__(code_dir, game_dir, seperator)
+        vocab_data = self._load_data(vocab_dir)
+        self.vocab_dict = {vocab_data['title'][i]: vocab_data['title_embeddings'][j] for i, j in zip(vocab_data['title'], vocab_data['title_embeddings'])}
 
-    for data in dataloader:
-        pos_sents, neg_sents, pos_embs, neg_embs = data
-        encs = model.encode(pos_sents)
-        print(encs.shape)
-
-if __name__ == "__main__":
-    testing_dataloader()
+    def _create_board_dict(self, data: json):
+        length = len(data['title'])
+        return {data['title'][f"{i}"]: data['title_embeddings'][f"{i}"] for i in range(length)}
+    
+    def _create_guess_dict(self, data: json):
+        length = len(data['title'])
+        return {data['title'][f"{i}"]: data['title_embeddings'][f"{i}"] for i in range(length)}
